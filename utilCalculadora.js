671a376369d743f7a9452897b7bdd46df0ef771c
IrvLH <hoppus.133.515@gmail.com>
2021-09-13 14:02:54 -0500

    Interfaz ver1.1, Javascript ver.8

--- utilCalculadora.js
+++ utilCalculadora.js
@@ -1,3 +1,41 @@
-function calcular(){
+var otraOper = true;
 
+function escribir(numero){
+    var pantalla = document.getElementById('OperacionTexto').value;
+    document.getElementById('OperacionTexto').value = pantalla + numero;
+    otraOper = true;
 }
+
+function escribirOper(operador){
+    if(otraOper){
+        var pantalla = document.getElementById('OperacionTexto').value;
+        document.getElementById('OperacionTexto').value = pantalla + " " +operador+ " ";
+        otraOper = false;
+    }
+}
+
+function limpiar(){
+    if(window.confirm("¿Borrar Operacion?")){
+        document.getElementById('OperacionTexto').value = "";
+    }
+}
+
+function limpiarUno(){
+    var pantalla = document.getElementById('OperacionTexto');
+    if(pantalla.value.charAt(pantalla.value.length-1) == " "){
+        pantalla.value = pantalla.value.substring(0, pantalla.value.length - 3);
+    }else{
+        pantalla.value = pantalla.value.substring(0, pantalla.value.length - 1);
+    }
+}
+
+function Calcular(){
+    window.alert('Calculando...'); 
+    document.getElementById('Resultado').value = "Algo"; 
+}
+
+/*var btnEnv = document.getElementById('Limpiar');
+btnEnv.addEventListener('click', function (){
+    window.alert('Un mensaje desde un listener');
+    btnEnv.style.color = "Green";
+});*/
